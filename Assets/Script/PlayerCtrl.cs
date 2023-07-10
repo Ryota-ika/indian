f3444446744f82c0e835fc440ebe2e74c8695a63
Ryota-ika <86831039+Ryota-ika@users.noreply.github.com>
2023-07-10 16:07:34 +0900

    7.10

--- Assets/Script/PlayerCtrl.cs
+++ Assets/Script/PlayerCtrl.cs
@@ -1,10 +1,17 @@
+//7/10
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+    //現在の道のオブジェクト
+    [SerializeField] public RoadObject currentRoad;
+    //次の道のオブジェクト
+    [SerializeField] public RoadObject nextRoad;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +21,30 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player.tag == "Road_1A")
+        /*if (player.tag == "Road_1A")
         {
         }
-            player.transform.position += new Vector3(0, 0, 1)*Time.deltaTime;
+            player.transform.position += new Vector3(0, 0, 1)*Time.deltaTime;*/
+        if (nextRoad != null)
+        {
+            if (currentRoad.roadDirection == nextRoad.roadDirection)
+            {
+                MovePlayer();
+            }
+            else
+            {
+                StopPlayer();
+            }
+        }
+    }
+
+    private void MovePlayer()
+    {
+        player.transform.position += new Vector3(0, 0, 1) * Time.deltaTime;
+    }
+
+    private void StopPlayer()
+    {
+        
     }
 }
