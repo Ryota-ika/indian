ea2af85a062645ca480443bcb7cb64474830ee3e
Ryota-ika <86831039+Ryota-ika@users.noreply.github.com>
2023-07-13 17:51:23 +0900

    ryota takahashi

--- Assets/Script/PlayerCtrl.cs
+++ Assets/Script/PlayerCtrl.cs
@@ -12,7 +12,9 @@ public class PlayerCtrl : MonoBehaviour
     [SerializeField] public RoadObject currentRoad;
     //次の道のオブジェクト
     [SerializeField] public RoadObject nextRoad;
-    float move_speed = 3f;
+    private float move_speed = 13f;
+    private bool turn_right = false;
+    private Quaternion targetRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,7 +30,7 @@ public class PlayerCtrl : MonoBehaviour
             player.transform.position += new Vector3(0, 0, 1)*Time.deltaTime;*/
 
         MovePlayer();
-        if (nextRoad != null)
+        /*if (nextRoad != null)
         {
             if (currentRoad.roadDirection == nextRoad.roadDirection)
             {
@@ -38,26 +40,38 @@ public class PlayerCtrl : MonoBehaviour
             {
                 StopPlayer();
             }
-        }
+        }*/
     }
 
     private void MovePlayer()
     {
-
-        player.transform.position += new Vector3(0, 0, move_speed) * Time.deltaTime;
+        if (turn_right)
+        {
+            //targetRotationからplayerのrotationに値を渡す。
+            player.transform.rotation = Quaternion.RotateTowards(player.transform.rotation, targetRotation, 90f * Time.deltaTime);
+            if (player.transform.rotation == targetRotation)
+            {
+                turn_right = false;
+                player.transform.position += player.transform.forward * move_speed * Time.deltaTime;
+            }
+        }
+        else
+        {
+            player.transform.position += player.transform.forward * move_speed * Time.deltaTime;
+        }
 
     }
 
     private void StopPlayer()
     {
-        
+        move_speed = 0f;
     }
 
     private void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.name == "Wall")
         {
-            move_speed = 0f;
+            StopPlayer();
         }
     }
     private void OnCollisionExit(Collision collision)
@@ -66,6 +80,15 @@ public class PlayerCtrl : MonoBehaviour
         {
             MovePlayer();
 
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "TurnRight")
+        {
+            turn_right = true;
+            //y軸を中心に回転
+            targetRotation = player.transform.rotation * Quaternion.Euler(0f, 90f, 0f);    
         }
     }
 }
