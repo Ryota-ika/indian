98a2d62728a9902474f1eb19d18d2b9ab334644f
TagamiMotohiro <105412324+TagamiMotohiro@users.noreply.github.com>
2023-07-18 14:59:24 +0900

    田上　エディターコードもうそろ完成＆一部prefub名変更

--- Assets/Editor/InitPieces.cs
+++ Assets/Editor/InitPieces.cs
@@ -19,7 +19,7 @@ public class InitPieces : EditorWindow
 	private void OnGUI()
 	{
 		//メニュー
-		GUILayout.Label("設定", EditorStyles.boldLabel);
+		GUILayout.Label("パズルと道を同期します", EditorStyles.boldLabel);
 		road_Scale = EditorGUILayout.FloatField("道一つの大きさ",road_Scale);
 		row = EditorGUILayout.IntField("ヨコ大きさ",row);
 		column = EditorGUILayout.IntField("タテ大きさ", column);
@@ -31,31 +31,28 @@ public class InitPieces : EditorWindow
 			//マネージャー取得
 			RoadManager roadScript = GameObject.Find("RoadManeger").GetComponent<RoadManager>();
 			Pazlcell pazleScript = GameObject.Find("pazl").GetComponent<Pazlcell>();
-			//道サイズ取得
-			Debug.Log(roads[0].transform.localScale);
 			//リスト格納探索用のVector2作成
-			Vector2 max = new Vector2(road_Scale,road_Scale);
-			Vector2 min = new Vector2(0, 0);
 			GameObject pazleParent = Instantiate(new GameObject(), Vector3.zero, Quaternion.identity);
 			//探索開始
-			for (int y=column-1;y>=0;y--){
+			for (int z=column-1;z>=0;z--){
 			for (int x=0;x<row;x++){
-				Debug.Log((min.x) + (road_Scale * x).ToString()+"から"+ ((max.x - 1) * x).ToString()+"の間を探索");
-				foreach (GameObject item in roads) {
-						
-					if (item.transform.position.x>=(min.x)&&item.transform.position.x<=(max.x-1)*x	
-						&&item.transform.position.y>=(min.y)&&item.transform.position.y<=(max.y-1)*y){
+				Debug.Log((road_Scale * x).ToString()+"から"+((road_Scale-1)+(road_Scale*x)).ToString()+"の間を探索");
+				foreach (GameObject item in roads) {		
+				if (item.transform.position.x>=road_Scale*x
+					&&item.transform.position.x<=(road_Scale-1)+(road_Scale*x)
+					&&item.transform.position.z>=road_Scale*z
+					&&item.transform.position.z<(road_Scale-1+(road_Scale*z))){
 						//道パネルの位置がmin以上max未満ならリストに取得
 						roadScript.AddToRoadList(item);
-						Debug.Log(x.ToString()+":"+y.ToString()+"に"+item.name+"を入れた");
+						Debug.Log(x.ToString()+":"+z.ToString()+"に"+item.name+"を入れた");
 						switch (item.GetComponent<Road_Cell>().GetRoadType())
 						{
 						//タイプ分けに応じてパネルを生成
 							case Road_Cell.RoadType.STRAIGHT:
-								pazleScript.AddToPieceList(CreatePanel				(StraightPanel,item.transform.rotation,pazleParent.transform).GetComponent<SensingPazl>());
+								pazleScript.AddToPieceList(CreatePanel				(StraightPanel,item.transform.rotation,pazleParent.transform,new Vector2(x,z)).GetComponent<SensingPazl>());
 							break;
 							case Road_Cell.RoadType.CORNER:
-								pazleScript.AddToPieceList(CreatePanel(curvePanel, item.transform.rotation,pazleParent.transform).GetComponent<SensingPazl>());
+								pazleScript.AddToPieceList(CreatePanel(curvePanel, item.transform.rotation,pazleParent.transform,new Vector2(x,z)).GetComponent<SensingPazl>());
 							break;
 							case Road_Cell.RoadType.VOID:
 								pazleScript.AddToPieceList(null);
@@ -68,9 +65,10 @@ public class InitPieces : EditorWindow
 		}
 		}
 	}
-	GameObject CreatePanel(GameObject panel,Quaternion rotation,Transform parent)
+	GameObject CreatePanel(GameObject panel,Quaternion rotation,Transform parent,Vector2 pos)
 	{
-	  GameObject g=Instantiate(panel,new Vector3(panel.transform.localScale.x,panel.transform.localScale.y,0),rotation);
+		Debug.Log(panel.transform.localScale);
+	  GameObject g=Instantiate(panel,new Vector3(panel.transform.localScale.x*pos.x,panel.transform.localScale.y*pos.y,0),rotation);
 		g.transform.parent = parent;
 		return g;
 	}
