af6f1343e5eda073da260197ca00fb82a5685451
TagamiMotohiro <105412324+TagamiMotohiro@users.noreply.github.com>
2023-09-16 19:30:28 +0900

    田上

--- Assets/RyotaTakahashi/Scripts/PlayerCtrl.cs
+++ Assets/RyotaTakahashi/Scripts/PlayerCtrl.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Timers;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
