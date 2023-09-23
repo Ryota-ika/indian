208421991ea8dea92ecc45144545bc7ca95abbb4
TagamiMotohiro <105412324+TagamiMotohiro@users.noreply.github.com>
2023-09-23 13:52:17 +0900

    田上　カーブが可変フレームレートに対応してなかったので対応させました。

--- Assets/RyotaTakahashi/Scripts/PlayerCtrl.cs
+++ Assets/RyotaTakahashi/Scripts/PlayerCtrl.cs
@@ -13,10 +13,12 @@ public class PlayerCtrl : MonoBehaviour
     public float moveSpeed = 13f;
     private bool turnRight = false;
     private bool turnLeft = false;
+    private Quaternion cullentRotation;
     public Quaternion targetRotation;
     [SerializeField] private Vector3 center = Vector3.zero;
     [SerializeField] private Vector3 axis = Vector3.up;
     [SerializeField] private float speed = 1f;
+    [SerializeField] private float curveCompleteTime;
     private Quaternion rightRotation;
     private Quaternion leftRotation;
     private float timer = 0;
@@ -28,14 +30,12 @@ public class PlayerCtrl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        /* Vector3 directionToCenter = center - player.transform.position;
-         _targetRotatioin = Mathf.Atan2(directionToCenter.z, directionToCenter.x) * Mathf.Rad2Deg - 90;*/
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
         //player.transform.position += new Vector3(0, 0, 1)*Time.deltaTime;
         if (turnRight)
         {
@@ -43,90 +43,14 @@ public class PlayerCtrl : MonoBehaviour
         }else if(turnLeft){
             MoveTurnLeft();
         }
-        else
-        {
-            MovePlayer();
-        }
     }
 
-
-
-    /*if (turnRight)
-            {
-                //targetRotationÇ©ÇÁplayerÇÃrotationÇ…ílÇìnÇ∑ÅB
-                player.transform.rotation = Quaternion.RotateTowards(player.transform.rotation, targetRotation, 90f * Time.deltaTime);
-                if (player.transform.rotation == targetRotation)
-                {
-                    turnRight = false;
-                    player.transform.position += player.transform.forward * moveSpeed * Time.deltaTime;
-                }
-
-            }
-            else
-            {
-                player.transform.position += player.transform.forward * moveSpeed * Time.deltaTime;
-            }*/
     public void MovePlayer()
     {
-
-        /*//player.transform.position += player.transform.forward * moveSpeed * Time.deltaTime;
-        if (turnRight)
-        {
-            //1âÒÇæÇØèàóùÇ∑ÇÈ
-            if (timer <= 0) { 
-            rotation = Quaternion.FromToRotation(transform.forward, transform.right);
-            }
-            if (timer <= 1f)
-            {
-                //í èÌÇÃâ~â^ìÆ
-                *//*float rotaionAngle = speed * Time.deltaTime;*/
-        /*player.transform.RotateAround(player.transform.position, axis, rotaionAngle);*//*
-
-        player.transform.position += player.transform.forward * moveSpeed * Time.deltaTime;
-        player.transform.rotation = Quaternion.Lerp(transform.rotation, rotation,timer);
-        timer = timer /rotationTime + Time.deltaTime;
-        Debug.Log(timer);
-    }
-    else
-    {
-        turnRight=false;
-    }
-}
-else
-{
-    player.transform.position += player.transform.forward * moveSpeed * Time.deltaTime;
-
-}*/
         player.transform.position += player.transform.forward * moveSpeed * Time.deltaTime;
 
     }
 
-    /*private void MoveTurnRight()
-    {
-        //1âÒÇæÇØèàóùÇ∑ÇÈ
-        if (timer <= 0)
-        {
-            rightRotation = Quaternion.FromToRotation(transform.forward, transform.right);
-            Debug.Log("âEê˘âÒ");
-            Debug.Log("rightRotation" + rightRotation.ToString());
-            Debug.Log("transform.forward" + transform.forward.ToString());
-        }
-        if (timer <= 1f)
-        {
-            //í èÌÇÃâ~â^ìÆ
-            *//*float rotaionAngle = speed * Time.deltaTime;*/
-            /*player.transform.RotateAround(player.transform.position, axis, rotaionAngle);*//*
-
-            player.transform.position += player.transform.forward * moveSpeed * Time.deltaTime;
-            player.transform.rotation = Quaternion.Lerp(transform.rotation, rightRotation, timer);
-            timer = timer / rightRotationTime + Time.deltaTime;
-            Debug.Log("transform.rotation"+player.transform.rotation.ToString());
-        }
-        else
-        {
-            turnRight = false;
-        }
-    }*/
 
     private void MoveTurnRight()
     {
@@ -135,16 +59,17 @@ else
         {
             //Debug.Log("transform.rotation" + player.transform.rotation.ToString());
             //leftRotation = Quaternion.FromToRotation(player.transform.forward, new Vector3(transform.right.x*(-1.0f), transform.right.y * (-1.0f), transform.right.z * (-1.0f)));
+            cullentRotation = transform.rotation;
             rightRotation = Quaternion.AngleAxis(curveAngle, Vector3.up) * transform.rotation;
             Debug.Log("-transformright" + (-transform.right).ToString());
 
         }
-        if (timer <= 8f)
+        if (timer <= curveCompleteTime)
         {
             //í èÌÇÃâ~â^ìÆ
-            timer += Time.deltaTime / rightRotationTime;
-            player.transform.position += player.transform.forward * moveSpeed * Time.deltaTime;
-            player.transform.rotation = Quaternion.Lerp(transform.rotation, rightRotation, timer);
+            timer += Time.deltaTime;
+            player.transform.rotation = Quaternion.Lerp(cullentRotation, rightRotation, 
+                (timer/curveCompleteTime)*(timer / curveCompleteTime));
         }
         else
         {
@@ -160,16 +85,16 @@ else
         {
             //Debug.Log("transform.rotation" + player.transform.rotation.ToString());
             //leftRotation = Quaternion.FromToRotation(player.transform.forward, new Vector3(transform.right.x*(-1.0f), transform.right.y * (-1.0f), transform.right.z * (-1.0f)));
+            cullentRotation = transform.rotation;
             leftRotation = Quaternion.AngleAxis(-curveAngle, Vector3.up)*transform.rotation;
             Debug.Log("-transformright"+(-transform.right).ToString());
          
         }
-        if (timer <= 8f)
+        if (timer <= curveCompleteTime)
         {
             //í èÌÇÃâ~â^ìÆ
-            timer += Time.deltaTime / leftRotationTime;
-            player.transform.position += player.transform.forward * moveSpeed * Time.deltaTime;
-            player.transform.rotation = Quaternion.Lerp(transform.rotation, leftRotation, timer);
+            timer += Time.deltaTime;
+            player.transform.rotation = Quaternion.Lerp(cullentRotation, leftRotation, timer/curveCompleteTime);
         }
         else
         {
--- Assets/Script/GoastClossing.cs
+++ Assets/Script/GoastClossing.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoastClossing : MonoBehaviour
 {
-    [Header("プレイヤー")]
+    enum DISTANCE_STATE
+    {
+        DISTANTE,
+        CLOSED,
+        VELY_CLOSED,
+        CLOSEST
+    }
+	[Header("プレイヤー")]
     [SerializeField]
     Transform player;
     [Header("幽霊")]
@@ -14,11 +22,19 @@ public class GoastClossing : MonoBehaviour
     [SerializeField]
     float goastApproach;
     [SerializeField]
-    UnityEngine.UI.Image bloodImage;
+    Image bloodImage;
     [SerializeField]
     Vector3 minImageSize;
     [SerializeField]
     Vector3 maxImageSize;
+    [SerializeField]
+    Image leftHand;
+    [SerializeField]
+    Image rightHand;
+    [SerializeField]
+    List<Transform> leftHandPositions;
+    [SerializeField]
+    List<Transform> rightHandPositions;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,18 +45,62 @@ public class GoastClossing : MonoBehaviour
     void Update()
     {
         float distance = Vector3.Distance(player.position,goast.transform.position);
-        if (distance < goastApproach&&goast.GetIsStart())
-        {
-            Debug.Log("幽霊が近づいてきた　残り約" + distance.ToString() + "m");
-            float size = distance / goastApproach;
-            //近づき具合の割合を出して画像の表示領域を調整
-            bloodImage.rectTransform.localScale = new Vector3(
-                Mathf.Lerp(minImageSize.x, maxImageSize.x, size),
-                Mathf.Lerp(minImageSize.y, maxImageSize.y, size),
-                1);
+		//if (distance < goastApproach&&goast.GetIsStart())
+		//{
+		//    Debug.Log("幽霊が近づいてきた　残り約" + distance.ToString() + "m");
+		//    float size = distance / goastApproach;
+		//    //近づき具合の割合を出して画像の表示領域を調整
+
+		//    bloodImage.rectTransform.localScale = new Vector3(
+		//        Mathf.Lerp(minImageSize.x, maxImageSize.x, size),
+		//        Mathf.Lerp(minImageSize.y, maxImageSize.y, size),
+		//        1);
+		//}
+		//else {
+		//    bloodImage.rectTransform.localScale = maxImageSize;
+		//}
+		switch (GetDistanceState(distance))
+		{
+            case DISTANCE_STATE.DISTANTE:
+                leftHand.gameObject.transform.position = leftHandPositions[0].position;
+                rightHand.gameObject.transform.position = rightHandPositions[0].position;
+                break;
+            case DISTANCE_STATE.CLOSED:
+                leftHand.gameObject.transform.position = leftHandPositions[1].position;
+                rightHand.gameObject.transform.position = rightHandPositions[1].position;
+                break;
+            case DISTANCE_STATE.VELY_CLOSED:
+                leftHand.gameObject.transform.position = leftHandPositions[2].position;
+                rightHand.gameObject.transform.position = rightHandPositions[2].position;
+                break;
+            case DISTANCE_STATE.CLOSEST:
+                leftHand.gameObject.transform.position = leftHandPositions[3].position;
+                rightHand.gameObject.transform.position = rightHandPositions[3].position;
+                break;
         }
-        else {
-            bloodImage.rectTransform.localScale = maxImageSize;
+        float image_Alpha = 1-(distance/goastApproach);
+        Debug.Log(image_Alpha);
+        Color right_Color = rightHand.color;
+        right_Color.a = image_Alpha;
+        rightHand.color = right_Color;
+        Color left_Color = rightHand.color;
+        left_Color.a = image_Alpha;
+        leftHand.color = left_Color;
+	}
+	DISTANCE_STATE GetDistanceState(float distance)
+	{
+        DISTANCE_STATE state=DISTANCE_STATE.DISTANTE;
+        float interval = goastApproach / 3;
+        if (distance < goastApproach && distance >= goastApproach - interval) {
+            state = DISTANCE_STATE.CLOSED;
+            return state;
+        } else if(distance < goastApproach - interval && distance >= goastApproach - interval*2){
+            state = DISTANCE_STATE.VELY_CLOSED;
+            return state;
+        } else if (distance < goastApproach - interval*2 && distance >= goastApproach - interval * 3){
+            state = DISTANCE_STATE.CLOSEST;
+            return state;
         }
+        return state;
     }
 }
