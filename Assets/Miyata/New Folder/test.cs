663c64d6d41957f3054c8d9696daa93593c90d84
Roro1005 <105412858+Roro1005@users.noreply.github.com>
2023-10-28 19:22:41 +0900

    mupdata update
    すべてのマップの調整
    ギミックの追加

--- Assets/Miyata/New Folder/test.cs
+++ Assets/Miyata/New Folder/test.cs
@@ -6,22 +6,39 @@ public class test : MonoBehaviour
 {
 
     public GameObject obj;
+    public GameObject ira;
+
     // Start is called before the first frame update
     void Start()
     {
         obj.SetActive(false);
+        ira.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     
     }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.name== "Player 1")
         {
             obj.SetActive(true);
+            ira.SetActive(true);
+         if(ira==true)
+            {
+                StartCoroutine(MyCoroutine());
+            }
+           
+
         }
+    }
+
+    private IEnumerator MyCoroutine()
+    {
+        yield return new WaitForSeconds(2.0f);
+        ira.SetActive(false);
     }
 }
